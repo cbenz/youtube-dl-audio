9ecdf09229a6d8b2ddbe757c7c45106468c09469
Christophe Benz <christophe.benz@gmail.com>
2016-10-23 01:22:24 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf dist
+elm-make Main.elm --warn --output=dist/index.html
+cd dist
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force git@github.com:cbenz/youtube-dl-audio.git master:gh-pages
